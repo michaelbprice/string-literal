2ba23ec900fc1f72a46d35901c13046945dc8e3f
Michael B. Price <michael.b.price.dev@gmail.com>
2015-05-09 19:24:34 -0500

    Adding a simple CMake build script

---
+++ version.cmake
@@ -0,0 +1,6 @@
+
+set (string_literal_test_VERSION_MAJOR 0)
+set (string_literal_VERSION_MINOR 1)
+set (string_literal_VERSION_REVISION 0)
+# set (string_literal_VERSION_PRERELEASE )
+# set (string_literal_VERSION_METADATA )
